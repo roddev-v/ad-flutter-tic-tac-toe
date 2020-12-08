71edef8f4fd3bcea3a9f715764fb9692fd0863af
Daniel Iovescu <iovescu.dani@gmail.com>
2020-12-08 18:15:29 +0200

    wip layout config

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,64 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Color> colors = List<Color>.generate(9, (int index) => Colors.white);
   String player = '1';
 
+  bool allBoxesSelected() {
+    for (int i = 0; i < colors.length; i++) {
+      if (colors[i] == Colors.white) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  String getWinner() {
+    if (!allBoxesSelected()) {
+      return '';
+    } else if (colors[0] == colors[1] && colors[1] == colors[2]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[3] == colors[4] && colors[4] == colors[5]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[6] == colors[7] && colors[7] == colors[8]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[0] == colors[3] && colors[3] == colors[6]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[1] == colors[4] && colors[4] == colors[7]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[2] == colors[5] && colors[5] == colors[8]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[0] == colors[4] && colors[4] == colors[8]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (colors[2] == colors[4] && colors[4] == colors[6]) {
+      return colors[0] == Colors.red ? 'Player 1 won' : 'Player 2 won';
+    } else if (allBoxesSelected()) {
+      return 'Nobody won';
+    }
+    return '';
+  }
+
+  Widget getBox(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          final bool hasBeenTouched = colors[index] != Colors.white;
+          if (!hasBeenTouched) {
+            player = isPlayerOne ? '1' : '2';
+            print('Current player $player');
+            colors[index] = isPlayerOne ? Colors.red : Colors.green;
+            isPlayerOne = !isPlayerOne;
+          }
+        });
+      },
+      child: AnimatedContainer(
+        padding: const EdgeInsets.all(3.0),
+        decoration: BoxDecoration(
+          color: colors[index],
+          border: Border.all(color: Colors.black),
+        ),
+        duration: const Duration(milliseconds: 500),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,30 +107,16 @@ class _MyHomePageState extends State<MyHomePage> {
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
               crossAxisCount: 3,
-              children: List<Widget>.generate(
-                9,
-                (int index) => GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      final bool hasBeenTouched = colors[index] != Colors.white;
-                      if (!hasBeenTouched) {
-                        player = isPlayerOne ? '1' : '2';
-                        print('Current player $player');
-                        colors[index] = isPlayerOne ? Colors.red : Colors.green;
-                        isPlayerOne = !isPlayerOne;
-                      }
-                    });
-                  },
-                  child: AnimatedContainer(
-                    padding: const EdgeInsets.all(3.0),
-                    decoration: BoxDecoration(
-                      color: colors[index],
-                      border: Border.all(color: Colors.black),
-                    ),
-                    duration: const Duration(milliseconds: 500),
-                  ),
-                ),
-              ),
+              children: List<Widget>.generate(9, (int index) => getBox(index)),
+            ),
+          ),
+          Container(
+            alignment: AlignmentDirectional.bottomStart,
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              getWinner(),
+              textAlign: TextAlign.start,
+              style: const TextStyle(fontSize: 20.0),
             ),
           ),
         ],
